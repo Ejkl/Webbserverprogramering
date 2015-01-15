ace535e7e22893f0064d813c6d2aafd70e536b0b
Ejkl <elin.linder@hotmail.com>
2015-01-15 10:24:11 +0100

    Tid
    Tiden syntes inte i file. Hade fel namn

--- Timestamp/assignment#4.php
+++ Timestamp/assignment#4.php
@@ -11,10 +11,10 @@ print_r($browser);
 /*Create file*/
 $my_file = 'file.txt';
 $handle = fopen($my_file, 'w') or die('Cannot open file:  '.$my_file);
-$data = date(("y/m/d"));
+$data = date("y/m/d");
 fwrite($handle, $data);
 $new_data = "\n". date("h:i:sa");
-fwrite($handle, $user_agent);
+fwrite($handle, $new_data);
 $new_data = "\n". $_SERVER['HTTP_USER_AGENT'];
 fwrite($handle, $new_data);
 ?>
